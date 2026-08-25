94fe8b375670293bba2e53a631f1b207a3a37679
Luis Feliz <84045948+Luis-Feliz@users.noreply.github.com>
2026-08-25 14:06:05 -0400

    Stopwatch test 1

--- timer.js
+++ timer.js
@@ -1,30 +1,74 @@
-let timerInterval;
+let startBtn = document.getElementById('start');
+let stopBtn = document.getElementById('stop');
+let resetBtn = document.getElementById('reset');
+let hour = 00;
+let minute = 00;
+let second = 00;
+let count = 00;
+startBtn.addEventListener('click', function () {
+    timer = true;
+    stopWatch();
+});
+stopBtn.addEventListener('click', function () {
+    timer = false;
+});
+resetBtn.addEventListener('click', function () {
+    timer = false;
+    hour = 0;
+    minute = 0;
+    second = 0;
+    count = 0;
+    document.getElementById('hr').innerHTML = "00";
+    document.getElementById('min').innerHTML = "00";
+    document.getElementById('sec').innerHTML = "00";
+    document.getElementById('count').innerHTML = "00";
+});
 
-function startTimer() {
-  let timeRemaining = document.getElementById("minutesInput").value * 60;
+function stopWatch() {
+    if (timer) {
+        count++;
 
-  updateDisplay(timeRemaining);
+        if (count == 100) {
+            second++;
+            count = 0;
+        }
 
-  clearInterval(timerInterval);
+        if (second == 60) {
+            minute++;
+            second = 0;
+        }
 
-  timerInterval = setInterval(() => {
-    timeRemaining--;
+        if (minute == 60) {
+            hour++;
+            minute = 0;
+            second = 0;
+        }
 
-    updateDisplay(timeRemaining);
+        let hrString = hour;
+        let minString = minute;
+        let secString = second;
+        let countString = count;
 
-    if (timeRemaining <= 0) {
-      clearInterval(timerInterval);
+        if (hour < 10) {
+            hrString = "0" + hrString;
+        }
 
-      alert("⏰Time's up!");
+        if (minute < 10) {
+            minString = "0" + minString;
+        }
+
+        if (second < 10) {
+            secString = "0" + secString;
+        }
+
+        if (count < 10) {
+            countString = "0" + countString;
+        }
+
+        document.getElementById('hr').innerHTML = hrString;
+        document.getElementById('min').innerHTML = minString;
+        document.getElementById('sec').innerHTML = secString;
+        document.getElementById('count').innerHTML = countString;
+        setTimeout(stopWatch, 10);
     }
-  }, 1000);
-}
-
-function updateDisplay(time) {
-  const minutes = Math.floor(time / 60);
-  const seconds = time % 60;
-
-  document.getElementById(
-    "countdownDisplay"
-  ).textContent = `${minutes}:${seconds}`;
 }
