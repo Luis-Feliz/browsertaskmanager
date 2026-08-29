dc695935e2562e9b4499966902d39e4f02c4f1c7
Luis Feliz <luisfeliz42100@gmail.com>
2026-08-29 16:49:37 -0400

    Add initial implementation of task manager with CSS and JavaScript files

---
+++ JavaScript/header.js
@@ -0,0 +1,33 @@
+class MyHeader extends HTMLElement
+{
+    connectedCallback()
+    {
+        this.innerHTML = 
+        `                
+            <table style="width: 100%; background-color: gray; text-align: center; position: sticky; top: 0;">
+            <th style= "text-align: left"><!-- 1 -->
+                <!-- Add all page content inside this div if you want the side nav to push page content to the right (not used if you only want the sidenav to sit on top of the page -->
+                <div id="main">
+                    <div class="container" onclick="myFunction(this); toggleNav()">
+                    <div class="bar1"></div>
+                    <div class="bar2"></div>
+                    <div class="bar3"></div>
+                    </div>
+                </div>
+                
+                <div id="mySidenav" class="sidenav">
+                    <a href="#">About</a>
+                    <a href="#">Services</a>
+                    <a href="#">Clients</a>
+                    <a href="#">Contact</a>
+                </div>
+            </th>
+            <th style="text-align: right;">
+                <button class="demo" onclick="" style="text-align: justify;"> New Project</button>
+            </th>
+            </table>
+        `
+    }
+}
+
+customElements.define('my-header', MyHeader);
